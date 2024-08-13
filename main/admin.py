75236f65c3d478cc2d23741857e73b15f6c996f8
alimehdi <alimehdi9651@gmail.com>
2024-08-13 14:55:06 +0530

    creating models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(recipe)
+admin.site.register(Department)
+admin.site.register(Student_ID)
+admin.site.register(Student)
--- main/models.py
+++ main/models.py
@@ -1,15 +1,49 @@
 from typing import Any
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-
 
 class recipe(models.Model):
     # user = models.ForeignKey(User, on_delete=models.SET_NULL, blank = True, null=True)
     recipe_name = models.CharField(max_length = 100)
     recipe_description = models.TextField()
     recipe_img = models.ImageField()
+
+    
+
+class Department(models.Model):
+    department = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return self.department
+    
+    class Meta:
+        ordering = ['department']
+
+
+class Student_ID(models.Model):
+    student_id = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return self.student_id
+    
+class Student(models.Model):
+    department = models.ForeignKey(Department, related_name = "depart", on_delete = models.CASCADE)
+    student_id = models.OneToOneField(Student_ID, related_name = "stu_id", on_delete = models.CASCADE)
+    student_name= models.CharField(max_length = 100)
+    student_email = models.EmailField(unique = True)
+    student_age = models.IntegerField(default = 18)
+    student_address = models.TextField()
+
+    def __str__(self) -> str:
+        return self.student_name
+    
+
+    class Meta():
+        ordering = ['student_name']
+        verbose_name = "student"
+
+        
 
     
